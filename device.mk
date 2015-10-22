4d37fd95f48fa6ad850974a22713361a5d248fda
scott <scott@ubuntu>
2015-10-21 23:34:51 -0400

    Clark: Audio: Multiple changes.. Read below
    * Boardconfig: Remove unsupported audio flags
    * Audio_platform: Correct audio paths
    * Audio_policy: Remove multiple support
    * Media_codecs and Media_profiles - Go back to stock
    * Device.mk - Copy audio_output and audio_effects to system/vendor/etc
      so they do not get overwritten by the build process.
    * Systemp.props - Cleanup Audio props
    
    Change-Id: I120849756e9e76abd12197d4047fc13c363fa6a8
    Signed-off-by: scott <scott@ubuntu>

--- device.mk
+++ device.mk
@@ -219,10 +219,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/acdbdata/Headset_cal.acdb:system/etc/acdbdata/Headset_cal.acdb \
     $(LOCAL_PATH)/audio/acdbdata/Speaker_cal.acdb:system/etc/acdbdata/Speaker_cal.acdb \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_ext_spkr.conf:system/etc/audio_ext_spkr.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
