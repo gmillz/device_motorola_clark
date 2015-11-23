c8117aea83dfb2860c9762a66881aa73ad3b4ad0
Griffin Millender <griffinn.millender@gmail.com>
2015-11-22 20:16:42 -0600

    Add fsg symlinks
    Change-Id: I66d4c3d640b2a35a45c5042d3f4cdd43987b8c8e

--- Android.mk
+++ Android.mk
@@ -29,4 +29,24 @@ LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
+# FSG symlinks
+FSG_IMAGES :=  \
+    0.img.gz 1.img.gz 2.img.gz 3.img.gz \
+    att.mbn sprint.mbn super.mbn tmo.mbn usc.mbn vzw.mbn \
+    clark_1.img.gz clark_2.img.gz clark_3.img.gz clark_att_2.img.gz \
+    clark_emea_1.img.gz clark_latam_1.img.gz clark_sprint_2.img.gz \
+    clark_super_1.img.gz clark_super_2.img.gz clark_super_3.img.gz \
+    clark_tmo_2.img.gz clark_verizon_2.img.gz kinzie_emea_2.img.gz \
+    kinzie_super_2.img.gz kinzie_super_3.img.gz kinzie_verizon_1.img.gz \
+    bundle_release
+
+FSG_SYMLINKS := $(addprefix $(TARGET_OUT)/rfs/msm/mpss/readonly/fsg/,$(notdir $(FSG_IMAGES)))
+$(FSG_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "FSG firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /fsg/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FSG_SYMLINKS)
+
 endif
