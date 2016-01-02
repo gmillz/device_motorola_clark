5cbcfebfaf9879322377a02d54ec8cab9080816e
HashBang <hashbang173@gmail.com>
2016-01-02 12:06:22 -0500

    some blob updates
    Change-Id: I165e8f43521b9bb2e181a2f8996bf39c3ef74745

--- device.mk
+++ device.mk
@@ -79,6 +79,9 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     libxml2
 
+# never dexopt the MotoSignature
+$(call add-product-dex-preopt-module-config,MotoSignatureApp,disable)
+
 # Live Wallpapers
 PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
