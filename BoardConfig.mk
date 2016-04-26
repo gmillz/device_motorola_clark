bc623fb279b1f4fc6c8911a62a03575c2c2c931e
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-04-26 10:06:36 -0500

    clark: remove twrp definitions
    Change-Id: I9f53ec896e324ea8b1a1d6d3026f2304d69d558d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -171,13 +171,5 @@ include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
-# TWRP definitions
-DEVICE_RESOLUTION := 1440x2560
-TW_BOARD_CUSTOM_GRAPHICS := ../../../$(DEVICE_PATH)/recovery/graphics.c
-TW_INCLUDE_L_CRYPTO := true
-TW_TARGET_USES_QCOM_BSP := true
-TW_NEW_ION_HEAP := true
-TW_INCLUDE_CRYPTO := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_RECOVERY_FSTAB = $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -10,13 +10,6 @@ LOCAL_SRC_FILES    := etc/fstab.qcom
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
-include #(CLEAR_VARS)
-LOCAL_MODULE       := twrp.fstab
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/twrp.fstab
-LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_OUT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.mmi.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
