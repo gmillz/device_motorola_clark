793196ed980ef83759a77e09b54fd727f5e5077f
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-04-27 09:46:24 -0500

    clark: inherit core_64_bit first
    Change-Id: I8dd25a1f505fcd992be2569a8ec17ad7ceb00e99

--- slim.mk
+++ slim.mk
@@ -1,11 +1,11 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, device/motorola/clark/device.mk)
-
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, device/motorola/clark/device.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
