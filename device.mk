e8ea2c703b9db2caff1880f572d0d1801506340d
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-06-16 08:48:37 -0500

    clark: add gps.conf and sap.conf
    * how the f did this ever work before?
    * from MPH24.49-18
    
    Change-Id: Ia114073e205938c5a862c7b8e5c1c7af328d532e

--- device.mk
+++ device.mk
@@ -81,6 +81,11 @@ PRODUCT_PACKAGES += \
     libqdutils \
     libqdMetaData
 
+# GPS
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/configs/sap.conf:system/etc/sap.conf
+
 # Init
 PRODUCT_PACKAGES += \
     fstab.qcom \
